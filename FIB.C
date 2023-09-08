f89c02bf24dfc2a92b6589396b65155706f28e2d
Sasidharch <112718627+Sasidharch@users.noreply.github.com>
2023-09-08 19:41:21 +0530

    Update FIB.C

--- FIB.C
+++ FIB.C
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+int main()
+{
+	int n,n1=0,n2=1,n3,i,sum=0;
+	n3=n1+n2;
+	scanf("%d",&n);
+	printf("%d",n1);
+	printf("%d",n2);
+	for(i=3;i<=n;i++)
+	{
+		printf("%d",n3);
+		n1=n2;
+		n2=n3;
+		n3=n1+n2;
+	}
+	return 0;
+}
